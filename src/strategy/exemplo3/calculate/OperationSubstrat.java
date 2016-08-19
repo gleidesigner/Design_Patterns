af14ff32ad8e15ddc294a8ba633b877ce571a716
Gleides Vinente <gleidesigner@msn.com>
2016-08-19 15:38:23 -0400

    add exemplo3 Strategy

---
+++ src/strategy/exemplo3/calculate/OperationSubstrat.java
@@ -0,0 +1,11 @@
+package strategy.exemplo3.calculate;
+
+/**
+ * Created by gleidesilva on 19/08/16.
+ */
+public class OperationSubstrat implements StrategyOperation {
+    @Override
+    public int doOperation(int num1, int num2) {
+        return (num1 - num2);
+    }
+}
---
+++ src/strategy/exemplo3/context/Context.java
@@ -0,0 +1,19 @@
+package strategy.exemplo3.context;
+
+import strategy.exemplo3.calculate.StrategyOperation;
+
+/**
+ * Created by gleidesilva on 19/08/16.
+ */
+public class Context {
+    //Criar uma propriedade que tem comportamento das operações
+    private StrategyOperation strategy;
+
+    public Context(StrategyOperation strategy) {
+        this.strategy = strategy;
+    }
+
+    public int execStrategy(int num1, int num2){
+        return strategy.doOperation(num1, num2);
+    }
+}
