09dcea3386e5241d1ac3fd1f78d9176b1285dbb6
Gleides Vinente <gleidesigner@msn.com>
2016-09-02 10:59:45 -0400

    updated Diagram 1 and 2

--- src/creational_patterns/factory/factory_method/exemplo2/factory_emissor/TestAppEmissor.java
+++ src/creational_patterns/factory/factory_method/exemplo2/factory_emissor/TestAppEmissor.java
@@ -1,11 +1,22 @@
 package creational_patterns.factory.factory_method.exemplo2.factory_emissor;
 
+import creational_patterns.factory.factory_method.exemplo2.emissor.Emissor;
+
 /**
  * Created by gleidesilva on 23/08/16.
  */
 public class TestAppEmissor {
     public static void main (String [] args){
+        FabricaEmissor emissorAssincrono = new EmissorAssincrono();
+        Emissor emissorA = emissorAssincrono.getEmissorMessagem(FabricaEmissor.SMS);
+        emissorA.envia("K19 - Treinamentos");
 
+        FabricaEmissor emissorSincrono = new EmissorSincrono();
+        Emissor emissorS = emissorSincrono.getEmissorMessagem(FabricaEmissor.EMAIL);
+        emissorA.envia("K19 - Treinamentos");
 
+        FabricaEmissor emissor = new FabricaEmissor();
+        Emissor emissorNormal = emissor.getEmissorMessagem(FabricaEmissor.JMS);
+        emissorA.envia("K19 - Treinamentos");
     }
 }
